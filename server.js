0b90d7f96ea7549af78ff72127294a658ee2cbda
THORISO <220674868@ump.ac.za>
2025-08-15 23:17:40 +0200

    Fix MongoDB database case conflict

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+<<<<<<< HEAD
+=======
+  
+>>>>>>> 1f1695f (Fix MongoDB database case conflict)
   })
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ DB Error:', err));
