859639f747f5285497e2fc5d26d68a50948c532e
THORISO <220674868@ump.ac.za>
2025-09-15 22:49:04 +0200

    minor changes for ststus

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/uploads', express.static(uploadsDir));
 
 // --- DB Connect ---
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ DB Error:', err));
 
